8071f5a4b5def8ff4b3082a2c35d85e24c4b5083
mmbhatk <kmanasvibhat@gmail.com>
2019-05-30 23:41:24 +0530

    Added 1.

--- 1/1.c
+++ 1/1.c
@@ -1,39 +1,29 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 
 int main()
 {
 	FILE* fp;
-	int i, string, strcheck, open, close, lineno = 0;
+	int i, lineno = 0, open = 0, openlineno;
 	char line[100];
-	fp = fopen("file.txt", "r");
-	if(fp==NULL)
-	{
-		printf("\nFile cannot be opened.\n");
-		exit(0);
-	}
-	printf("\nFile successfully opened.\n");
-	while(fgets(line, sizeof(line), fp)!=NULL)
+
+	fp = fopen("source.txt", "r");
+
+	while(fgets(line, sizeof(line), fp))
 	{
 		lineno++;
-		open = close = 0;
-		string = strcheck = 0;
-		for(i = 0; i<strlen(line); i++)
+		for(i = 0; i < strlen(line); i++)
 		{
-			if(line[i]=='"')
+			if(open) printf("%c", line[i]);
+			if(!open && line[i] == '\'')
 			{
-				string = 1;
-				if(open==0 && close==0) open = 1;
-				else if(open==1 && close==0) close = 1;
-				else if(open==1 && close==1) close = 0;
+				if((i+2)<strlen(line) && line[i+2]=='\'') {printf("\n%sValid character constant %c on line %d.\n\n", line, line[i+1], lineno); i+=2;}
+				else {printf("\n%sInvalid character constant on line %d.\n\n", line, lineno);}
 			}
+			if(!open && line[i] == '"') {open = 1; openlineno = lineno; printf("\n%c", line[i]);}
+			else if(open && line[i] == '"') {open = 0; printf("\nString begins on line %d and ends on line %d.\n\n", openlineno, lineno);}
 		}
-		if(open==1 && close==0)
-		{
-			printf("\nUnterminated string on line %d.\n", lineno);
-			strcheck = 1;
-		}
-		if(string==1 && strcheck==0) printf("\nString usage validated on line %d.\n", lineno);
 	}
-	return 0;
+	if(open) printf("\nUnterminated string begins on line %d.\n", openlineno);
 }
