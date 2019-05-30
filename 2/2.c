49a02e4d1416ce76ccf1c99af2f02ced49dd1904
mmbhatk <kmanasvibhat@gmail.com>
2019-05-30 23:00:56 +0530

    Added 2.

--- 2/2.c
+++ 2/2.c
@@ -1,46 +1,28 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 
 int main()
 {
 	FILE* fp;
-	int i, lineno = 0;
-	int open = 0, close = 0, openlineno, closelineno;
+	int lineno = 0, open = 0, openlineno;
 	char line[100];
+
 	fp = fopen("source.txt", "r");
-	if(fp==NULL)
-	{
-		printf("\nError opening file.\n");
-		exit(1);
-	}
-	printf("\nFile opened successfully.\n\n");
-	while(fgets(line, sizeof(line), fp)!=NULL)
+	if(fp) {printf("\nFile opened successfully.\n\n");}
+
+	while(fgets(line, sizeof(line), fp))
 	{
 		lineno++;
-		if(!open && strstr(line, "//"))
+		if(open) printf("\n%s", line);
+		if(!open && strstr(line, "//")) printf("\n%s\nSingle line comment on line: %d\n\n", line, lineno);
+		if(!open && strstr(line, "/*")) {open = 1; openlineno = lineno; printf("\n%s", line);}
+		if(open && strstr(line, "*/"))
 		{
-			printf("\n%s", line);
-			printf("\nSingle line comment on line %d.\n\n", lineno);
-		}
-		else
-		{
-			if(open==1 && close==0) printf("\n%s", line);
-			if(strstr(line, "/*") && open ==0)
-			{
-				open = 1;
-				close = 0;
-				openlineno = lineno;
-				printf("\n%s", line);
-			}
-			if(strstr(line, "*/") && open==1 && close==0)
-			{
-				open = 0;
-				close = 1;
-				closelineno = lineno;
-				printf("\nComment begins on line %d and terminates on line %d.\n\n", openlineno, closelineno);
-			}
+			printf("\nMultiline comment begins on line %d and ends on line %d.\n\n", openlineno, lineno);
+			open = 0;
 		}
 	}
-	if(open==1 && close==0) printf("\nUnterminated comment begins on line %d.\n\n", openlineno);
-	return 0;
+
+	if(open) printf("\nUnterminated comment begins on line %d.\n", openlineno);
 }
